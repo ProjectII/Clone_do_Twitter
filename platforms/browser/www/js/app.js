743817f8db6482c175fac453cf025e1e4839d6c2
wagner rodrigo da silva <wagnerrodrigo.pan@gmail.com>
2018-04-11 15:40:20 -0300

    atualização

--- platforms/browser/www/js/app.js
+++ platforms/browser/www/js/app.js
@@ -29,13 +29,39 @@ $('#formmensagem').on('submit', function () {
     return false;
 });
 
-function enviarMensagem(mensagem) {
+$('#formmensagem').on('submit', function () {
+    var sucesso = function (data) {
+        console.log("deu certo!", data);
+    };
+
+    var erro = function (data) {
+        console.log("deu errado!", data);
+    };
+
+    enviarMensagem(montarMensagem(), sucesso, erro);
+
+    return false;
+});
+
+$('mensagem').on('input',function () {
+    var mensagem = $('#mensagem').val();
+    if (mensagem.length>0 && mensagem.length<=140) {
+        // tudo certo
+        $('#enviar').removeClass('disabled');
+    }
+    else{
+        $('#enviar').addClass('disabled');
+    }
+});
+
+function enviarMensagem(mensagem, sucesso, erro) {
     $.ajax({
         url:'http://service-api.herokuapp.com/mensagens',
         type: 'post',
         contentType: 'application/json',
-        data: JSON.stringify(mensagem)
-    });
+        data: JSON.stringify(mensagem),
+       
+    })
 }
 
 function montarMensagem() {
@@ -47,7 +73,5 @@ function montarMensagem() {
         },
         "dataCriacao": new Date(),
         "local": null
-    };
-
-
+    }
 }
