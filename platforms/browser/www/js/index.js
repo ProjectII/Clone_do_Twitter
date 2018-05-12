dc747dd1d8be868a0ce0ef7c81931e8b35d0d36b
Wagner Rodrigo da Silva <wagnerrodrigo.pan@gmail.com>
2018-05-12 09:33:23 -0300

    criando lista de mensagens

--- platforms/browser/www/js/index.js
+++ platforms/browser/www/js/index.js
@@ -1,4 +1,16 @@
 
-$(function(){
-	carregarMensagens(console.log, console.error);
-});
+// $(function(){
+// 	carregarMensagens(console.log, console.error);
+// });
+
+function montaTemplate(mensagem) {
+	var template = $("#mensagem_template").html();
+	console.log(mensagem);
+	return template.replace('{{mensagem}}', mensagem.mensagem)
+	.replace('{{nome}}',mensagem.autor.nome)
+	.replace('{{login}}',mensagem.autor.login)
+	.replace('{{local}}','Desconhecido')
+	.replace('{{data}}',mensagem.dataCriacao);
+
+
+}
