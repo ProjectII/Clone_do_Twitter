ddd82aff0d91925bafeae6132cb023daa15e76e8
Wagner Rodrigo da Silva <wagnerrodrigo.pan@gmail.com>
2018-05-12 08:47:48 -0300

    refatorando arquivos, criando index.js e messages.js

--- platforms/browser/www/js/app.js
+++ platforms/browser/www/js/app.js
@@ -1,5 +1,26 @@
-//Inicializa todos os componentes do template.
+$(function () {
+    $(".dropdown-button").dropdown();
+    $(".button-collapse").sideNav();
+    $('ul.tabs').tabs();
+       
+    $('#loading').modal({
+      dismissible: false
+    });
+     Materialize.updateTextFields();
+     const url = new URL(location);
+     const params = new URLSearchParams(url.search);
+     if (params.has("toast")) {
+        Materialize.toast(params.get("toast"), 1500);
+     } 
+});
 
 
+function showLoader() {
+    $('#loading').modal("open");
+}
+
+function hideLoader() {
+    $('#loading').modal("close");
+}
 
 
